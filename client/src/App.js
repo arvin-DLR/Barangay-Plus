d3172450e774e97113dad037444d78ca1d98ebb9
Arvin Delos Reyes <amdelosreyes@iskolarngbayan.pup.edu.ph>
2021-05-20 04:19:06 +0800

    setting styles and redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch();
+
+    }, []);
+
     return (
         <div>
             <h1>Hello World!</h1>
