19e10fa721e720146348aac3c1bc57bd271be67e
gekkota89 <72088858+gekkota89@users.noreply.github.com>
2021-05-21 02:37:03 +0800

    updated server.js

--- Server/server.js
+++ Server/server.js
@@ -8,11 +8,15 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: "30mb", extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://brgyplus:bluesoulx89@barangay-plus.zmk0p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+const CONNECTION_URI = 'mongodb+srv://brgyplus:bluesoulx89@barangay-plus.zmk0p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, () => console.log('Server running on port: ' + PORT)))
     .catch((error) => console.log(error.message));
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully!');
+})
 
 mongoose.set('useFindAndModify', false);
 
