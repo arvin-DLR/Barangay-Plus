459b3823bd70b4571ef53b43edb02f8edd7f8d24
Arvin Delos Reyes <amdelosreyes@iskolarngbayan.pup.edu.ph>
2021-06-11 23:50:29 +0800

    files update

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import Container from '@material-ui/core/Container';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Signup';
@@ -11,16 +14,18 @@ import Login from './components/auth/Signin';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Container maxWidth='xs'>
-            <Navbar/>
-            <Route exact path="/" component={Landing}/>
-            <Route exact path="/register" component ={Register}/>
-            <Route exact path="/login" component={Login}/>
-          </Container>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Container maxWidth='xs'>
+              <Navbar/>
+              <Route exact path="/" component={Landing}/>
+              <Route exact path="/register" component ={Register}/>
+              <Route exact path="/login" component={Login}/>
+            </Container>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
