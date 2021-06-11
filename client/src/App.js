3a7199cbd3b57d4ac071d3ee116b69e42d528600
Arvin Delos Reyes <amdelosreyes@iskolarngbayan.pup.edu.ph>
2021-06-11 23:45:22 +0800

    part 2 update
    no style yet but most of the part 2 were done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import React, { useEffect } from 'react';
-import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React, { Component } from "react";
+import "./App.css";
+import Container from '@material-ui/core/Container';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Signup';
+import Login from './components/auth/Signin';
 
-import useStyles from './styles';
-
-const App = () => {
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch();
-
-    }, []);
-
+class App extends Component {
+  render() {
     return (
-        <div>
-            <h1>Hello World!</h1>
+      <Router>
+        <div className="App">
+          <Container maxWidth='xs'>
+            <Navbar/>
+            <Route exact path="/" component={Landing}/>
+            <Route exact path="/register" component ={Register}/>
+            <Route exact path="/login" component={Login}/>
+          </Container>
         </div>
-    )
-};
-
-
+      </Router>
+    );
+  }
+}
 export default App;
