4477ea809cb3dd5a90f0a451c37f4ff7cde8d51b
Arvin Delos Reyes <amdelosreyes@iskolarngbayan.pup.edu.ph>
2021-06-12 13:46:39 +0800

    revision

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
+<<<<<<< HEAD
 import React, { Component } from "react";
 import "./App.css";
 import Container from '@material-ui/core/Container';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import { Provider } from "react-redux";
-import store from "./store";
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -14,19 +12,43 @@ import Login from './components/auth/Signin';
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Router>
-          <div className="App">
-            <Container maxWidth='xs'>
-              <Navbar/>
-              <Route exact path="/" component={Landing}/>
-              <Route exact path="/register" component ={Register}/>
-              <Route exact path="/login" component={Login}/>
-            </Container>
-          </div>
-        </Router>
-      </Provider>
+      <Router>
+        <div className="App">
+          <Container maxWidth='xs'>
+            <Navbar/>
+            <Route exact path="/" component={Landing}/>
+            <Route exact path="/register" component ={Register}/>
+            <Route exact path="/login" component={Login}/>
+          </Container>
+        </div>
+      </Router>
     );
   }
 }
+=======
+import React, { useEffect } from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+
+import useStyles from './styles';
+
+const App = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch();
+
+    }, []);
+
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
+};
+
+
+>>>>>>> parent of 3a7199c (part 2 update)
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,25 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
  import React from 'react';
+=======
+import React from 'react';
+>>>>>>> parent of 3a7199c (part 2 update)
+=======
+  
+import React from 'react';
+>>>>>>> parent of 459b382 (files update)
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>, document.getElementById('root'));
